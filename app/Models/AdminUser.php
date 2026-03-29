2bf7176ba6a59164f2882b6f018503659d37898a
kombuwa <isuruudana@gmail.com>
2026-03-29 15:27:53 +0000

    Initial commit - Laravel 11 + Filament 3 + Spatie Permission setup

---
+++ app/Models/AdminUser.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
+
+class AdminUser extends Authenticatable implements FilamentUser
+{
+    use HasRoles, Notifiable;
+
+    protected $table = 'admin_users';
+    protected $guard_name = 'admin';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'is_active',
+        'created_by',
+        'last_login_at',
+    ];
+
+    protected $hidden = ['password', 'remember_token'];
+
+    protected $casts = [
+        'password'      => 'hashed',
+        'is_active'     => 'boolean',
+        'last_login_at' => 'datetime',
+    ];
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->is_active === true;
+    }
+}
